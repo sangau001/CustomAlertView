ee9b982e8cdcc74d55b86c0248c2525d68133413
sangau001 <sanju.dutta@gmail.com>
2015-01-02 21:01:10 +0530

    Add CustomAlertView class

---
+++ CustomAlertView.h
@@ -0,0 +1,13 @@
+//
+//  CustomAlertView.h
+//  
+//
+//  Created by Sanjeev Dutta on 1/2/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomAlertView : NSObject
+
+@end
---
+++ CustomAlertView.m
@@ -0,0 +1,13 @@
+//
+//  CustomAlertView.m
+//  
+//
+//  Created by Sanjeev Dutta on 1/2/15.
+//
+//
+
+#import "CustomAlertView.h"
+
+@implementation CustomAlertView
+
+@end
